862c28b58aa04169a95180f88f647fcae5ae4afb
Adam Talbot <adamtalbot93@googlemail.com>
2023-06-29 22:10:31 +0000

    feat: add utilities to get owners using generics

---
+++ util/util.go
@@ -0,0 +1,169 @@
+package util
+
+import (
+	"context"
+	"reflect"
+
+	"github.com/kubespress/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
+)
+
+// GetObjects returns the objects of the selected type that are "owners" of the specified object. It will skip objects
+// marked as "controllers"
+func GetOwners[T client.Object](ctx context.Context, c client.Client, scheme *runtime.Scheme, obj client.Object) ([]T, error) {
+	// Get new object
+	parent, err := NewObject[T]()
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the GVK
+	gvk, err := apiutil.GVKForObject(parent, scheme)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create slice to hold results
+	results := make([]T, 0)
+
+	// Look for parent
+	for _, ref := range obj.GetOwnerReferences() {
+		// Skip all controller references
+		if pointer.BoolDeref(ref.Controller, false) {
+			continue
+		}
+
+		// Skip invalid kinds
+		if ref.Kind != gvk.Kind {
+			continue
+		}
+
+		// Parse group version of reference
+		gv, err := schema.ParseGroupVersion(ref.APIVersion)
+		if err != nil {
+			return nil, errors.Enrich(err,
+				errors.Wrap("could not get apiversion of object from schema"),
+				errors.WithStack(),
+			)
+		}
+
+		// Skip invalid groups
+		if gv.Group != gvk.Group {
+			continue
+		}
+
+		// Create key for the object
+		key := client.ObjectKey{
+			Namespace: obj.GetNamespace(),
+			Name:      ref.Name,
+		}
+
+		// Clone the object
+		parent := parent.DeepCopyObject().(T)
+
+		// Get the object
+		if err := c.Get(ctx, key, parent); err != nil {
+			return nil, err
+		}
+
+		// Append to results
+		results = append(results, parent)
+	}
+
+	// Return found objects
+	return results, nil
+}
+
+// GetController returns the object that controls the resource. If the object has no controller of the specified type
+// then nil is returned.
+func GetController[T client.Object](ctx context.Context, c client.Client, scheme *runtime.Scheme, obj client.Object) (T, error) {
+	// Get new object
+	parent, err := NewObject[T]()
+	if err != nil {
+		return *new(T), err
+	}
+
+	// Get the GVK
+	gvk, err := apiutil.GVKForObject(parent, scheme)
+	if err != nil {
+		return *new(T), err
+	}
+
+	// Look for parent
+	for _, ref := range obj.GetOwnerReferences() {
+		// Skip all non controller reference
+		if !pointer.BoolDeref(ref.Controller, false) {
+			continue
+		}
+
+		// Skip invalid kind
+		if ref.Kind != gvk.Kind {
+			continue
+		}
+
+		// Parse group version of reference
+		gv, err := schema.ParseGroupVersion(ref.APIVersion)
+		if err != nil {
+			return parent, errors.Enrich(err,
+				errors.Wrap("could not get apiversion of object from schema"),
+				errors.WithStack(),
+			)
+		}
+
+		// Skip invalid groups
+		if gv.Group != gvk.Group {
+			continue
+		}
+
+		// Get key of object
+		key := client.ObjectKey{
+			Namespace: obj.GetNamespace(),
+			Name:      ref.Name,
+		}
+
+		// Get the object
+		if err := c.Get(ctx, key, parent); err != nil {
+			return *new(T), err
+		}
+
+		// Return no error
+		return parent, nil
+	}
+
+	return *new(T), nil
+}
+
+func NewObject[T client.Object]() (T, error) {
+	// Create zero object
+	var zeroObj T
+
+	// Get the object as an interface for type inference (cant do that with
+	// generics)
+	obj := client.Object(zeroObj)
+
+	// Special case for handling unstructured objects
+	if _, ok := obj.(runtime.Unstructured); ok {
+		return zeroObj, errors.Enrich(
+			errors.New("cannot construct unstructured object"),
+			errors.WithStack(),
+		)
+	}
+
+	// Get type of object
+	typ := reflect.TypeOf(zeroObj)
+
+	// If its not a pointer, return an error
+	if kind := typ.Kind(); kind != reflect.Ptr {
+		if kind == reflect.Invalid {
+			return *new(T), errors.Errorf("expected pointer, but got invalid kind")
+		}
+		return *new(T), errors.Errorf("expected pointer, but got %v type", typ)
+	}
+
+	// Create new instance of the object
+	return reflect.New(typ.Elem()).Interface().(T), nil
+}
